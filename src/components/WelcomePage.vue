283ee9d2482b20252243ce66348f50859261f832
Blair Burton <blair.burton@outlook.com>
2018-08-22 15:08:44 +1200

    Add general WelcomePage;

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-jumbotron :header="header" :lead="leader">
+    <p>{{ information }}</p>
+    <b-btn variant="primary" :href="contactmail">Contact Developer</b-btn>
+  </b-jumbotron>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => {
+    return {
+      header: 'CopperCask',
+      leader: 'A simple character manager for tabletop RPG\'s',
+      contactmail: 'mailto:blair.burton@outlook.com'
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+  .jumbotron {
+    background-color: #FFF;
+    margin: 0 2em;
+  }
+</style>
