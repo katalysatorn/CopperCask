9bc5e5a6208f01d3d6c8d93c4cbbd632a7ab5cc4
Blair Burton <blair.burton@outlook.com>
2018-08-22 15:09:36 +1200

    Add App.vue, main.ts;

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Navbar brand="CopperCask" />
+    <WelcomePage />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+import WelcomePage from './components/WelcomePage.vue'
 
 @Component({
   components: {
-  HelloWorld,
+  Navbar,
+  WelcomePage,
   },
   })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import './registerServiceWorker'
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: App,
+      meta: {
+        title: 'CopperCask',
+        metaTags: [{
+          name: 'A simple character manager for tabletop RPG\'s',
+          content: 'CopperCask Application'
+        }]
+      }
+    }
+  ]
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
