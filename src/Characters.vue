51917c67e912537ebb8d9be9c7713b7f9c5f6ed0
Blair Burton <blair.burton@outlook.com>
2018-09-18 12:47:19 +1200

    Added example Battle, Characters, NewCharacter files

---
+++ src/Characters.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Navbar brand="CopperCask" />
+    <notifications group="notifGroup" position="bottom right" />
+    <p>Character Page</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from './components/Navbar.vue'
+
+export default Vue.extend({
+  components: {
+    Navbar
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
