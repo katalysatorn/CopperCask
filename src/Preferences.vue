4cb3a012323f3072e72ef6107c1ed44a7fe4c00b
Blair Burton <blair.burton@outlook.com>
2018-11-10 15:57:02 +1300

    Add Preferences Page

--- src/Preferences.vue
+++ src/Preferences.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
-    <p>Preferences/Settings Page</p>
+    <b-jumbotron
+      header="Settings/Preferences"
+      lead="Here you can edit your Settings and Preferences for CopperCask">
+      <form>
+        <b-button
+          disabled
+          class="pad"
+          :pressed.sync="offlineMode"
+          variant="outline-primary">
+          Offline Mode
+        </b-button>
+        <br />
+
+        <label for="languageSelectBox">Language: </label>
+        <b-form-select
+          class="pad"
+          v-model="currentLanguage"
+          :options="supportedLanguages"
+          id="languageSelectBox" />
+
+      </form>
+    </b-jumbotron>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import Navbar from './components/Navbar.vue'
 
 export default Vue.extend({
-  components: {
-    Navbar
+  data: () => {
+    return {
+      offlineMode: true,
+      currentLanguage: 'en',
+      supportedLanguages: [
+        { value: 'en', text: 'English' },
+        { value: 'sv', text: 'Svenska', disabled: true }
+      ]
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-
+  form {
+    width: 50%;
+  }
+  form * {
+    margin-bottom: 1em;
+  }
 </style>
